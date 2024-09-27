87d6cd497ea01630eb5991d86dacc34b665fe427
Petrik Diak <diak@petrik.hu>
2024-09-27 15:51:11 +0200

    keresesQuery done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { query } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello() {
@@ -16,21 +16,21 @@ export class AppController {
 
   @Get('quotes')
   @Render('index')
-  getProfile(){
+  getProfile() {
     return {
       quotes: quotes.quotes
     }
   }
 
 
-  randomIntFromInterval(min, max) { 
+  randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   @Get('randomQuote')
   @Render('random')
-  randomQuote(){
-    const rndInt = this.randomIntFromInterval(0,29);
+  randomQuote() {
+    const rndInt = this.randomIntFromInterval(0, 29);
     return {
       quote: quotes.quotes[rndInt].quote,
       author: quotes.quotes[rndInt].author
@@ -39,15 +39,15 @@ export class AppController {
 
   @Get('topAuthor')
   @Render('table')
-  topAuthors(){
+  topAuthors() {
     const dict = new Map();
     for (let index = 0; index < quotes.quotes.length; index++) {
-      if(dict.has(quotes.quotes[index].author)){
+      if (dict.has(quotes.quotes[index].author)) {
         let num = dict.get(quotes.quotes[index].author);
         num++;
         dict.set(quotes.quotes[index].author, num);
       }
-      else{
+      else {
         dict.set(quotes.quotes[index].author, 1)
       }
     }
@@ -60,30 +60,47 @@ export class AppController {
   @Get('quote/:id')
   @Render('quote')
   oneQuote(@Param('id') id: number) {
-  for (let index = 0; index < quotes.quotes.length; index++) {
-    if(quotes.quotes[index].id == id){
-      return {
-        quote: quotes.quotes[index].quote,
-    
+    for (let index = 0; index < quotes.quotes.length; index++) {
+      if (quotes.quotes[index].id == id) {
+        return {
+          quote: quotes.quotes[index].quote,
+
+        }
       }
     }
   }
-}
 
   @Get('deleteQuote/:id')
   @Render('delete')
   deleteQuote(@Param('id') id: number) {
     for (let index = 0; index < quotes.quotes.length; index++) {
-      if(quotes.quotes[index].id == id){
+      if (quotes.quotes[index].id == id) {
         quotes.quotes.splice(index, 1);
         return {
           message: 'Sikeres törlés'
+        }
+      }
+      return {
+        message: 'Ismeretlen idézet'
       }
     }
-    return{
-      message: 'Ismeretlen idézet'
+  }
+
+  @Get('quote')
+  @Render('kereses')
+  searchQuote(@Query('search') search: string) {
+    const allQuote = [];
+    for (let index = 0; index < quotes.quotes.length; index++) {
+      if (quotes.quotes[index].quote.toLowerCase().includes(search.toLowerCase())) {
+        allQuote.push(quotes.quotes[index]);
+      }
+    }
+    console.log(allQuote);
+    return {
+      allQuote
     }
   }
+
 
 
   /*
@@ -94,13 +111,13 @@ export class AppController {
       bgColor
     }
   }
-
+ 
   #jegkremek = [
     {nev: 'Vanília', ar: 200},
     {nev: 'Csoki', ar: 250},
     {nev: 'Eper', ar: 300},
   ]
-
+ 
   @Get('jegkrem')
   @Render('jegkremLista')
   jegkremLista(){
@@ -108,7 +125,7 @@ export class AppController {
       jegkremek: this.#jegkremek
     }
   }
-
+ 
   @Get('jegkrem/:id')
   @Render('jegkrem')
   jegkrem(@Param('id') id: string){
@@ -117,5 +134,5 @@ export class AppController {
       nev: 'CitromosCsoda',
       ar: 1000
    }*/
-  }
+
 }
